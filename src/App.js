e72ab1cd0ff70fbc82c773a86cc78919be340578
ramanakunam16 <ramanakunam16@gmail.com>
2023-11-22 17:50:30 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import { get } from "prompt";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  useEffect(
+    function () {
+      getAdvice();
+    },
+
+    []
+  );
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => {
+      c++;
+      return c;
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        hello welcome to MYapp .<br />
+        Get advice by clicking below button
+      </h1>
+
+      <h2>Your Advice:</h2>
+
+      <h4 className="advice">{advice}</h4>
+
+      <button onClick={getAdvice}>getAdvice</button>
+
+      <h2>you have read {count} advices by now.</h2>
     </div>
   );
 }
